cc2fd84ffb83946adfe03342393f80f95f6e1c24
Zainab Alayande <alayandezainab64@gmail.com>
2024-12-13 02:42:17 +0100

    chore: added login controller

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,27 @@
+package controller;
+
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class LoginController {
+
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to LinkedIn Login!";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(Authentication authentication) {
+        OAuth2User oauthUser = (OAuth2User) authentication.getPrincipal();
+        String name = oauthUser.getAttribute("localizedFirstName");
+        String email = oauthUser.getAttribute("emailAddress");
+        return "Hello, " + name + "! Your email is " + email;
+    }
+
+
+}
