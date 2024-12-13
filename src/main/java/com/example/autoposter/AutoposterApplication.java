728c0abf6132988b27527592168643edd1eff8e1
Zainab Alayande <alayandezainab64@gmail.com>
2024-12-13 02:40:26 +0100

    chore: added security config file

---
+++ src/main/java/com/example/autoposter/AutoposterApplication.java
@@ -0,0 +1,13 @@
+package com.example.autoposter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoposterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoposterApplication.class, args);
+    }
+
+}
